1a2d8980084273b0a8f5769323c19b31e77d24f1
websir1 <99841136+websir1@users.noreply.github.com>
2024-03-14 22:33:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const registeredNames = ["usfer", "Mustafa", "Max"]; // Add all 30 names here
 function checkRegistration() {
     const nameInput = document.getElementById('nameInput').value;
     const resultDiv = document.getElementById('result');
+const glassSound = document.getElementById('glassSound');
 
+    // Play the glass sound
+    glassSound.play();
     if (registeredNames.includes(nameInput)) {
         resultDiv.innerHTML = '<span style="color: green;">Registered ✅</span>';
     } else {
